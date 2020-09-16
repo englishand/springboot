e715954ff41aa58f67f76736b3fb6f932b2e7918
zhy <18306425328@163.com>
2020-09-16 09:51:07 +0800

    修改securityconfiguration,拒绝jwt

--- src/main/java/com/zhy/test/test/Test.java
+++ src/main/java/com/zhy/test/test/Test.java
@@ -1,5 +1,10 @@
 package com.zhy.test.test;
 
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Test {
 
     public static void main(String[] args){
@@ -12,5 +17,27 @@ public class Test {
         System.out.println(path.endsWith(seprator));
         System.out.println(pattern.startsWith(seprator,7));
         System.out.println(path.startsWith(seprator,5));
+        int a=1;
+        for (int i=0;i<10;i++){
+            if(a==i) {
+                continue;
+            }else {
+                System.out.println("*****************");
+            }
+            System.out.println("a="+a);
+        }
+
+        Date date1 = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        try {
+            Date date2 = sdf.parse("20200918");
+            System.out.println(date2.compareTo(date1));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        String acc = "2345678.123456789";
+        BigDecimal decimal = new BigDecimal(acc).setScale(4,BigDecimal.ROUND_HALF_UP);
+        System.out.println(decimal);
     }
 }
