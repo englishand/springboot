bf65ad27479204d3c7b4f10638dae3d8204ace8f
zhy <18306425328@163.com>
2020-12-31 16:42:37 +0800

    优化jdk四个线程池和ThreadPoolTaskExecutor线程池

--- src/main/java/com/zhy/test/enableAsync/PoolConfig.java
+++ src/main/java/com/zhy/test/enableAsync/PoolConfig.java
@@ -1,5 +1,6 @@
 package com.zhy.test.enableAsync;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,13 +10,19 @@ import org.springframework.util.StringUtils;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
+@Slf4j
 @Configuration
 @EnableAsync(proxyTargetClass = true)
 public class PoolConfig {
 
+    /**
+     * ThreadPoolTaskExecutor和ThreadPoolEexcutor区别：
+     * 1.参数不一样 ThreadPoolExecutor重要参数：corePoolSize,maximumPoolSize,keepAliveTime,TimeUnit(时间单位),
+     *  BlockingQueue（缓存队列）,ThreadFactory(线程工厂),RejectedExecutionHandler（执行拒绝策略的对象）
+     * 2.ThreadPoolExecutor是JDK中的JUC,ThreadPoolTaskExecutor是spring core包中的。后者是对前者进行了封装处理。
+     */
     @Bean("poolexecutor")
     public ThreadPoolTaskExecutor executor(){
 
@@ -38,15 +45,49 @@ public class PoolConfig {
         return hte;
     }
 
-    @Bean("poolexecutor2")
-    public ThreadPoolTaskExecutor executor2(){
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(20);
-        executor.setQueueCapacity(200);
-        executor.setKeepAliveSeconds(60);//秒
-        executor.setThreadNamePrefix("poolexecutor2-");
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+    /**
+     * 可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收复用线程，则新建。
+     * 优点：1.工作线程的数量无限制，可灵活的往线程池添加线程
+     *      2.如果长时间没有往线程池添加任务，则该工作线程会自动终止。
+     * 注：控制任务的数量，大量的线程运行，可能会导致系统瘫痪。
+     * @return 一个Executor，提供管理终端的方法和生成Future的方法，用于追踪一个或多个异步任务的进度。
+     */
+    @Bean
+    public ExecutorService cachedThreadPool(){
+        ExecutorService executor = Executors.newCachedThreadPool();
+        log.info("该线程池是否已经终止：{}",executor.isTerminated());
+        return executor;
+    }
+
+    /**
+     *  指定线程数量的线程池，如果线程数量达到最大，则将提交的任务存入池队列中
+     *  线程池在空闲时，不会释放空闲线程。定长线程池的大小最好根据系统资源进行设置：Runtime.getRuntime().availableProcessors();
+     *  优点：节省创建线程所耗的开销。
+     * @return
+     */
+    @Bean
+    public ExecutorService fixedThreadPool(){
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        return executorService;
+    }
+
+    /**
+     * 定长线程池、支持定时以及周期性的任务执行,ScheduledExecutorService比Timer更安全，功能更强大
+     * @return
+     */
+    @Bean
+    public ScheduledExecutorService scheduleThreadPool(){
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(10);
+        return executor;
+    }
+
+    /**
+     * 单例化的executor,创建及使用唯一的线程执行任务。按照指定顺序：FIFO,LIFO,优先级执行
+     * @return
+     */
+    @Bean
+    public ExecutorService singleThreadExecutor(){
+        ExecutorService executor = Executors.newSingleThreadExecutor();
         return executor;
     }
 }
--- src/main/java/com/zhy/test/enableAsync/TestPoolConfig.java
+++ src/main/java/com/zhy/test/enableAsync/TestPoolConfig.java
@@ -1,30 +1,91 @@
 package com.zhy.test.enableAsync;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class TestPoolConfig {
 
     @Autowired
-    private PoolThread poolThread;
+    private PoolConfig poolConfig;
 
+    /**
+     *  测试ThreadPoolTaskExecutor
+     * @throws Exception
+     */
+    @Async("poolexecutor")//指定线程池名
+    public void one() throws Exception{
+        log.info("测试一");
+        Long start = System.currentTimeMillis();
+        Thread.sleep(100);
+        Long end = System.currentTimeMillis();
+        log.info("用时: "+(end-start)+"毫秒");
+    }
+
+    /**
+     * 测试ThreadPoolTaskExecutor
+     * @throws Exception
+     */
     @Test
     public void main() throws Exception{
         try {
+            TestPoolConfig poolThread = new TestPoolConfig();
             poolThread.one();
-            poolThread.two();
-            poolThread.three();
             Thread.sleep(3000);//为了让其他线程进行。
 //            Thread.currentThread().join();//  阻塞当前调用线程，其他线程调用完后才调用
             System.out.println("主线程方法");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 测试缓存线程池CachedThreadPool
+     */
+    @Test
+    public void main2(){
+        ExecutorService executor = poolConfig.cachedThreadPool();
+        for (int i=0;i<10;i++){
+            try {
+                Thread.sleep(i*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int finalIndex = i;
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(finalIndex +"nihao"+System.currentTimeMillis());
+                }
+            });
+        }
+    }
+
+    /**
+     * 测试scheduleThreadPool
+     */
+    @Test
+    public void main4(){
+        ScheduledExecutorService executor = poolConfig.scheduleThreadPool();
+        executor.schedule(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("");
+            }
+        },2000, TimeUnit.SECONDS);
     }
 
 }
