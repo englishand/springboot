882742a876fd4d275bed3977bea0bcf3e1beb998
zhy <18306425328@163.com>
2021-04-22 14:03:59 +0800

    添加集合的概念--集合.md

--- src/main/java/com/zhy/test/Test.java
+++ src/main/java/com/zhy/test/Test.java
@@ -4,16 +4,27 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.Date;
+import java.util.*;
+
 @Slf4j
 public class Test {
+
+    private static class Node<K,V>{
+        int hash;
+        K key;
+        V value;
+        Node<K,V> next;
+        Node(int hash,K key,V value,Node<K,V> next){
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
 
     public static void main(String[] args){
         String sf  = String.format("[%tT] [%s] %s",new Date(),"info","你好");
         log.info(sf);
-
-        String aaa = "ABCD";
-        log.info(aaa.toLowerCase());
 
         String phone = "1234    ";
         try {
@@ -21,5 +32,59 @@ public class Test {
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+        Map m1 = new HashMap();
+        m1.put("age",3);m1.put("age",7);
+        Map m2 = new HashMap();m2.put("age",5);
+        Map m3 = new HashMap();m3.put("age",2);
+        Map m4 = new HashMap();m4.put("age",1);
+        Map m5 = new HashMap();m5.put("age",6);
+        Map m6 = new HashMap();m6.put("age",4);
+        List list = new ArrayList();
+        list.add(m1);
+        list.add(m2);
+        list.add(m3);list.add(m4);list.add(m5);list.add(m6);list.add(m1);list.add(m2);
+        list.sort(new Comparator<HashMap>() {
+            @Override
+            public int compare(HashMap o1, HashMap o2) {
+                Integer i1 = (Integer) o1.get("age");
+                Integer i2 = (Integer) o2.get("age");
+                return i1.compareTo(i2);
+            }
+        });
+        log.info(list.toString());
+
+
+        //也就是说list.size是值实际占用的大小，而其存放元素的容器transient Object[] elementData初始大小是可以通过定义控制的。
+        List list1 = new ArrayList(4);
+        list1.add(list);
+        list1.add("");
+        log.info(list1.size()+"");
+
+        int newCapacity = 6;
+        newCapacity = newCapacity+(newCapacity >> 1);
+        log.info(newCapacity+"");
+
+        List list2 = new LinkedList<String>();
+        list2.add("111111");list2.add("222222");list2.add("333333");list2.add("444444");list2.add("5555555");list2.add("666666");
+        log.info(list2.get(1).toString());
+
+        Set set = new HashSet();
+        boolean result = set.add(1);
+        boolean result2 = set.add(1);
+        log.info(result+"|"+result2+"|"+set.toString());
+
+        StringBuffer sb = new StringBuffer();
+        for (int x=0;x<20;x++){
+            Object key = x;
+            Object val = x;
+            int h;
+            int hash = (h = key.hashCode()) ^ (h >>> 16);
+            log.info(key.hashCode()+"|"+hash+"|"+(3059181 >>> 16)+"|"+(3059181^(3059181 >>> 16)));
+
+            int n=16;//hashmap的初始值
+            int i = (n - 1) & hash;
+            sb.append("{"+i+","+x+"}|");
+        }
+        log.info("hashmap表示为{下标，key}：{}",sb.toString());
     }
 }
